8f88fc896d6e9631be07685b4de7df0da3294965
rajkumaryadav199 <kuraj10101999@gmail.com>
2022-11-08 19:07:38 +0530

    components createrd

---
+++ Form-react/form/src/Components/FormInput.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const FormInput = () => {
+    const [data, setData]=useState({
+        email:"",
+        password:"",
+        id:Date()
+    })
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FormInput
---
+++ Form-react/form/src/Components/FormList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FormList
